78262977c29f0901a2ae27f8c9b94aeb523cdf83
Alliette Dortelus <allietted@gmail.com>
2023-06-21 10:51:51 -0400

    added-Gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './styles/App.scss';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
